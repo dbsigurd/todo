a6e06d395c3b36bc2c288e83d811a062ba2ca306
dbsigurd <dbsigurd@ualberta.ca>
2014-09-26 14:51:16 -0600

    trying to get saving

--- src/Data/FileDataManager.java
+++ src/Data/FileDataManager.java
@@ -21,9 +21,12 @@ public class FileDataManager implements IDataManager{
 	private static final String FILENAME = "file.sav"; // for to dos
 	private static final String FILENAME2 = "archfile.sav"; // for archives
 	ToDoList toDos = ToDoList.getInstance();
+	private Context context;
+
 	ArchivedToDoList archivedToDos = ArchivedToDoList.getInstance();
+	@SuppressWarnings("unchecked")
 	public void loadToDo(int x) {
-		
+		Log.i("Devon","TRIED LOADING");
 		//ArrayList<AbstractTweet> lts = new ArrayList<AbstractTweet>();
 		if (x==0){
 			ArrayList<ToDoItem> lts = new ArrayList<ToDoItem>();
@@ -48,9 +51,10 @@ public class FileDataManager implements IDataManager{
 			try {
 				FileInputStream fis = new FileInputStream(FILENAME2);
 				ObjectInputStream ois = new ObjectInputStream(fis);
-
+				
 				lts = (ArrayList<ToDoItem>) ois.readObject();
 				for (int i=0; i<lts.size();i++){
+					Log.i("ToDos", lts.get(i).getToDo());
 					archivedToDos.add(lts.get(i));
 				}
 			} catch (Exception e) {
@@ -62,11 +66,12 @@ public class FileDataManager implements IDataManager{
 	}
 	
 	public void saveToDos(List<ToDoItem> lts,int x) {
+		x=0;
 		Log.i("Devon","TRIED SAVING");
 		if(x==0){ //save to dos
 			try {
 				
-				FileOutputStream fos = new FileOutputStream(FILENAME);
+				FileOutputStream fos = context.openFileOutput(FileDataManager.FILENAME,Context.MODE_PRIVATE);
 				ObjectOutputStream oos = new ObjectOutputStream(fos);
 				oos.writeObject(lts);
 				fos.close();
